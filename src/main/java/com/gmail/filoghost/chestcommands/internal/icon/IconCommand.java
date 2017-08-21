b052a9f226cd2975cb766b57e923a4a900cd222a
Gabriele C <sgdc3.mail@gmail.com>
2017-08-21 21:57:29 +0200

    Process PlaceholderAPI placeholders in commands

--- src/main/java/com/gmail/filoghost/chestcommands/internal/icon/IconCommand.java
+++ src/main/java/com/gmail/filoghost/chestcommands/internal/icon/IconCommand.java
@@ -1,5 +1,6 @@
 package com.gmail.filoghost.chestcommands.internal.icon;
 
+import com.gmail.filoghost.chestcommands.bridge.PlaceholderAPIBridge;
 import com.gmail.filoghost.chestcommands.config.AsciiPlaceholders;
 import com.gmail.filoghost.chestcommands.internal.Variable;
 import org.bukkit.entity.Player;
@@ -24,14 +25,16 @@ public abstract class IconCommand {
     }
 
     public String getParsedCommand(Player executor) {
-        if (containedVariables.isEmpty()) {
-            return command;
-        }
-
         String commandCopy = command;
+
+        // Process builtin variables
         for (Variable variable : containedVariables) {
             commandCopy = commandCopy.replace(variable.getText(), variable.getReplacement(executor));
         }
+
+        // Process PlaceholderAPI variables
+        commandCopy = PlaceholderAPIBridge.replace(executor, command);
+
         return commandCopy;
     }
 
